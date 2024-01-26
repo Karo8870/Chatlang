92b55cf9e0ed29655e22e25f8de73f578b9ac878
Karo8870 <77077774+Karo8870@users.noreply.github.com>
2024-01-26 17:54:00 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ const { WebSocketServer } = require('ws');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-const api = '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';
-
 prisma.$connect();
 
 prisma.message.findMany().then(m => console.log(m));
